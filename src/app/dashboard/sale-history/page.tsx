517cd55a705c6a98a7344026f925f82314fb51a2
Jawad Fadel <jawadfadel662@gmail.com>
2025-01-09 19:08:43 +0200

    Remove sales data fetching from dashboard sale history page

--- src/app/dashboard/sale-history/page.tsx
+++ src/app/dashboard/sale-history/page.tsx
@@ -4,11 +4,10 @@ import { getSales } from "@/services/sale-service";
 import React, { Suspense } from "react";
 
 export default async function page() {
-  const sales = await getSales();
   const customers = await getCustomers();
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <SalesTable data={sales} customers={customers as any} />
+      <SalesTable customers={customers as any} />
     </Suspense>
   );
 }
